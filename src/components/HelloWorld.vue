3109ec8933905bf514a0f703df6e1a792a56ec44
zizoko1337 <95056942+zizoko1337@users.noreply.github.com>
2022-07-17 04:33:36 +0200

    dodane życie i kasa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,8 +13,10 @@
     </div>
     <div>
       <button @click="clientOrderMethod">Start game</button>
-      <p>Client: {{clientDialog}}</p>
-      <p>You: Here is your order {{ arrOutput }}</p>
+      <h2>Client: {{ clientDialog }}</h2>
+      <h2>You: Here is your order {{ arrOutput }}</h2>
+      <h2>Your lives: {{ playerLives.join('') }}</h2>
+      <h2>Your money: {{ playerMoney.join('') }}</h2>
     </div>
   </div>
 </template>
@@ -26,13 +28,29 @@ export default {
     return {
       clientOrder: null,
       clientDialog: null,
-      clientDialogs: ["Can i get aaaaaa... ","I order ","Give me ","Im hungry, i want ","Just give me ","I could eat some ","I want to buy ","I would like some ","I would kill for some ","Gimme some ","Gimme some of those ","Shut up and take my money for "],
+      clientDialogs: [
+        'Can i get aaaaaa... ',
+        'I order ',
+        'Give me ',
+        'Im hungry, i want ',
+        'Just give me ',
+        'I could eat some ',
+        'I want to buy ',
+        'I would like some ',
+        'I would kill for some ',
+        'Gimme some ',
+        'Gimme some of those ',
+        'Shut up and take my money for ',
+      ],
       userMethod: null,
       answer: null,
       gameArr: ['🍕', '🍔', '🍟', '🌭', '🍿', '🥓', '🍗', '🌮', '🥐'],
       tempArr: ['🍕', '🍔', '🍟', '🌭', '🍿', '🥓', '🍗', '🌮', '🥐'],
       arrOutput: null,
       previousOrder: null,
+      playerLives: ['💖', '💖', '💖'],
+      playerMoney: [],
+      gameOn: false,
     };
   },
 
@@ -40,8 +58,26 @@ export default {
     submitMethod() {
       this.answer = `${JSON.stringify(this.gameArr)}.${this.userMethod}`;
       this.arrOutput = eval(this.answer);
+      if (Array.isArray(this.clientOrder)) {
+        if (this.clientOrder.join(',') == this.arrOutput) {
+          for (var i = 0; i < this.clientOrder.length; i++) {
+            this.playerMoney.push('💰');
+          }
+        } else {
+          this.playerLives.shift();
+        }
+      } else {
+        this.clientOrder == this.arrOutput
+          ? this.playerMoney.push('💰')
+          : this.playerLives.shift();
+      }
+      if (this.playerLives.length < 1) {
+        console.log('GAME OVER');
+        this.gameOn = false;
+      }
     },
     clientOrderMethod() {
+      this.gameOn = true;
       const typeOfOrder = Math.floor(Math.random() * 6);
       switch (typeOfOrder) {
         case 0:
@@ -61,38 +97,43 @@ export default {
         case 2:
         case 3:
           // random item
-          this.clientOrder = this.gameArr.at(Math.floor(Math.random() * this.gameArr.length));
+          this.clientOrder = this.gameArr.at(
+            Math.floor(Math.random() * this.gameArr.length)
+          );
           this.noRepeat(this.clientOrder);
           this.clientDialog = this.randomDialog() + this.clientOrder;
           this.previousOrder = this.clientOrder;
           break;
         case 4:
-          //couple items
-          var start = Math.floor(Math.random() * (this.gameArr.length -1));
+          //many items
+          var start = Math.floor(Math.random() * (this.gameArr.length - 1));
           var end = 10;
-          while((start + end > this.gameArr.length) && end > 0){
+          while (start + end > this.gameArr.length && end > 0) {
             end = Math.ceil(Math.random() * this.gameArr.length);
           }
-          this.clientOrder = this.tempArr.splice(start,end);
+          this.clientOrder = this.tempArr.splice(start, end);
           this.noRepeat(this.clientOrder);
           this.clientDialog = this.randomDialog() + this.clientOrder;
           this.tempArr = ['🍕', '🍔', '🍟', '🌭', '🍿', '🥓', '🍗', '🌮', '🥐'];
           this.previousOrder = this.clientOrder;
           break;
         default:
-        // code block
       }
     },
-    randomDialog(){
-      let x = Math.ceil(Math.random() * (this.clientDialogs.length -1));
-      return this.clientDialogs[x];
+    randomDialog() {
+      let dialogNumber = Math.ceil(
+        Math.random() * (this.clientDialogs.length - 1)
+      );
+      return this.clientDialogs[dialogNumber];
     },
-    noRepeat(order){
-      if(order == this.previousOrder){
-        this.clientOrder = this.gameArr.at(Math.floor(Math.random() * this.gameArr.length));
-        console.log(order +" changed to " + this.clientOrder);
+    noRepeat(order) {
+      if (order == this.previousOrder) {
+        this.clientOrder = this.gameArr.at(
+          Math.floor(Math.random() * this.gameArr.length)
+        );
+        console.log(order + ' changed to ' + this.clientOrder);
       }
-    }
+    },
   },
 };
 </script>
